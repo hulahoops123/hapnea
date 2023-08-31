c4a0dd3253f16b1a7e7e65f88b00298e30fcefb6
hulahoops123 <jdmichael.giveitback@gmail.com>
2023-08-31 16:53:17 +0200

    deleted sigin page. added animation

--- components/userBubble.vue
+++ components/userBubble.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="flex flex-col ring-2 rounded-full  px-4 pb-2  items-center">
         <img :src="props.userData.photoURL" class="rounded-full my-2 w-20 h-20" alt="" referrerpolicy="no-referrer">
-        <p class="text-2xl font-raleway font-bold my-2 text-gray-700">{{ props.userData.userName }}</p>
+            <!-- <p @click="showUserNameInput=true" v-if="!showUserNameInput" class="  text-2xl font-raleway font-bold my-2 text-gray-700 relative ">{{ props.userData.userName }}</p> -->
+            <input @click="newUserNameError = false" @change="checkUserName($event.target.value)" class="flex justify-center text-center  text-2xl font-raleway font-bold my-2 text-gray-700 bg-transparent rounded-md " :value="props.userData.userName" type="text">
+            <p v-if="newUserNameError" class="text-xs">too short! 🙅</p>
         <div v-for="category in Object.keys(props.disciplines)" class="grid space-x-6 mr-4 space-y-2 grid-cols-3 w-2/3 items-center">
             <p class=" font-staatliches text-2xl text-neutral-500 h-full w-full flex items-end justify-end   ">{{ category }}</p>
             <input v-if="props.disciplines[category].unit === 'distance'"
@@ -28,8 +30,15 @@
     </div>
 </template>
 <script setup>
-const emits = defineEmits(['input-changed'], 'himom');
+const newUserNameError = ref(false);
+const emits = defineEmits(['input-changed', 'username-changed'], 'himom');
 const props = defineProps(["userData", "adminData", "disciplines"]);
+function checkUserName(newUserName){
+    if (newUserName == props.userData.userName) return;
+    if (newUserName.length < 4) {newUserNameError.value = true; return};
+    emits('username-changed',newUserName);
+    console.log('change username now');
+}
 function formatTime(mmss,category){
     let mm = mmss.split(':')[0];
     let ss = mmss.split(':')[1];
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
         raleway: ['Raleway'],
         staatliches: ['Staatliches'],
       },
+      keyframes: {
+        bubbleRise:{
+          '0%': { transform: ' scale(0) translateY(200%)' },
+          // '50%': { transform: 'scale(3)' },
+          '100%': { transform: ' scale(1) translateY(0%)' },
+        }
+      },
+      animation: {
+        bubbleRise: 'bubbleRise 2s linear  1',
+      }
     },
   },
   plugins: [],
